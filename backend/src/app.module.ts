1284d8e812fd42009c75ebb5be236d9ca48de326
anastasiia.tesliuk <anastasiia.tesliuk@gmail.com>
2022-03-14 20:52:38 +0200

    User model is added

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,15 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-     MongooseModule.forRoot('mongodb://localhost:27017/Stream'),
-  ]
-})
-
-@Module({
-  imports: [],
+    MongooseModule.forRoot('mongodb://localhost:27017/officeManagement', {
+      useNewUrlParser: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './interfaces/user.interface';
+import { CreateUserDTO } from './dto/create-user.dto';
+
+@Injectable()
+export class UserService {
+  constructor(@InjectModel('User') private readonly userModel: Model<User>) {}
+
+  async addUser(createUserDTO: CreateUserDTO): Promise<User> {
+    const newUser = await this.userModel(createUserDTO);
+    return newUser.save();
+  }
+
+  async getUser(userID): Promise<User> {
+    const user = await this.userModel.findById(userID).exec();
+    return user;
+  }
+
+  async getUsers(): Promise<User[]> {
+    const users = await this.userModel.find().exec();
+    return users;
+  }
+}
