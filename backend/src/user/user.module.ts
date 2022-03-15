02ba22910ab870c1879e29c6ae0f5e345fd9fade
anastasiia.tesliuk <anastasiia.tesliuk@gmail.com>
2022-03-15 12:52:10 +0200

    Added Auth0 authentication

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,14 +1,28 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema } from './schemas/user.schema';
+import { AuthenticationMiddleware } from 'src/common/authentication.middleware';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
-  ], 
+  imports: [MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])],
   providers: [UserService],
   controllers: [UserController],
 })
-export class UserModule {}
+export class UserModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): MiddlewareConsumer | void {
+    consumer
+      .apply(AuthenticationMiddleware)
+      .forRoutes(
+        { method: RequestMethod.POST, path: '/user/post' },
+        { method: RequestMethod.PUT, path: '/user/edit' },
+        { method: RequestMethod.DELETE, path: '/user/delete' },
+      );
+  }
+}
